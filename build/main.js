4c8f63b06d8e4f4a9f654a1b60b6573ffb846e59
Anders Boyum <Acboyum@gmail.com>
2023-09-04 11:19:21 -0500

    added build file, tho I don't know if that's good practice or if I should normally include that and not include the src files?

---
+++ build/main.js
@@ -0,0 +1,29 @@
+"use strict";
+function moveSecHand(degree) {
+    const secondHand = document.querySelector('.sec-hand');
+    secondHand.style.transform = `rotate(${degree + 90}deg)`;
+}
+function moveMinHand(degree) {
+    const minHand = document.querySelector('.min-hand');
+    minHand.style.transform = `rotate(${degree + 90}deg)`;
+}
+function moveHourHand(degree) {
+    const hourHand = document.querySelector('.hour-hand');
+    hourHand.style.transform = `rotate(${degree + 90}deg)`;
+}
+function setClock() {
+    let date = new Date;
+    let seconds = date.getSeconds();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    // need to figure out how to map each hand to the times I get from date constructor.
+    // I need to translate each of these numbers to the 360deg's of the clock.
+    const secondsDeg = (seconds / 60) * 360;
+    const minDeg = (min / 60) * 360;
+    const hourDeg = (hour / 24) * 360;
+    moveSecHand(secondsDeg);
+    moveMinHand(minDeg);
+    moveHourHand(hourDeg);
+    console.log('This is our date:', seconds);
+}
+setInterval(setClock, 1000);
